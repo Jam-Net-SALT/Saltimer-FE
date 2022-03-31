3a2e1946894120c782c3648766c57b48350a4a8f
SpookySnek <carleric.olsson@gmail.com>
2022-03-31 16:01:54 +0200

    Added blur effect behind log in card.
    - This will not blur footer/header etc.

--- src/Pages/Auth/index.tsx
+++ src/Pages/Auth/index.tsx
@@ -1,21 +1,33 @@
-import { Card, Tabs } from "@mantine/core";
+import {Card, Tabs} from "@mantine/core";
 import React from "react";
 import LoginForm from "../../components/LoginForm";
 import SignupForm from "../../components/SignupForm";
 
 const AuthPage = () => {
-  return (
-    <Card shadow='xl' sx={{ width: 450, '@media(max-width: 500px)': {width: 300} }} >
-      <Tabs color='pink' grow position='center' variant='outline' >
-        <Tabs.Tab label='Log in'>
-          <LoginForm />
-        </Tabs.Tab>
-        <Tabs.Tab label='Sign up'>
-          <SignupForm />
-        </Tabs.Tab>
-      </Tabs>
-    </Card>
-  );
+    return (
+        <>
+            <div id="overlay" style={{
+                backdropFilter: 'blur', 
+                position: 'fixed', 
+                width: '100%', 
+                height: '100%',
+                top: '0',
+                left: '0',
+                right: '0',
+                bottom: '0'
+            }}/>
+            <Card shadow='xl' sx={{width: 450, '@media(max-width: 500px)': {width: 300}}}>
+                <Tabs color='pink' grow position='center' variant='outline'>
+                    <Tabs.Tab label='Log in'>
+                        <LoginForm/>
+                    </Tabs.Tab>
+                    <Tabs.Tab label='Sign up'>
+                        <SignupForm/>
+                    </Tabs.Tab>
+                </Tabs>
+            </Card>
+        </>
+    );
 };
 
 export default AuthPage;
