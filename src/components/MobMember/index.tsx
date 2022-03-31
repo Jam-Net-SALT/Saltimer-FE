43acb098c0eac3329697c88babac34979b64aed2
SpookySnek <carleric.olsson@gmail.com>
2022-03-31 21:59:31 +0200

    Implemented placeholder using initals of name for avatars upon invalid URL.
    - This could later be used as default avatar, if HTTPS validation is modified.
    - Enlarged avatars.

--- src/components/MobMember/index.tsx
+++ src/components/MobMember/index.tsx
@@ -3,13 +3,16 @@ import React from "react";
 import { MobMemberProp } from "./type";
 import useStyle from "./style";
 
+
 const MobMember = ({ user }: MobMemberProp) => {
   const { classes } = useStyle();
   return (
-    <Grid style={{ width: "fit-content" }}>
+    <Grid style={{ width: 'fit-content' }}>
       <Grid.Col>
         <Center>
-          <Avatar src={user.imageUrl} alt="it's me" radius='xl' size='lg' />
+          <Avatar src={user.imageUrl} alt={user.name} radius={100} size={150} color='violet' >
+              {user.name.match(/\b(\w)/g)}
+          </Avatar>
         </Center>
       </Grid.Col>
       <Grid.Col>
